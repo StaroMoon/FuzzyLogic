eee2656ccc721602aa5b87d63acca9cc0a75ac63
Neungbodee Kongcharoensukying <neungbodee@hotmail.com>
2015-11-03 21:25:36 +0700

    Fuzzy Final

--- src/fuzzylogic/FuzzyLogic.java
+++ src/fuzzylogic/FuzzyLogic.java
@@ -1,8 +1,5 @@
 package fuzzylogic;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
@@ -10,9 +7,6 @@ import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 public class FuzzyLogic {
 
-    static JTextField t1;
-    static JTextField t2;
-    static JFrame frame;
     public static void main(String[] args) throws Exception{
         // Load from 'FCL' file
         String fileName = "tipper.fcl";
@@ -23,21 +17,14 @@ public class FuzzyLogic {
             System.err.println("Can't load file: '" + fileName + "'");
             return;
         }
-        
-        /*t1 = new JTextField();
-        t2 = new JTextField();
-        setValue(t1,"weight");
-        setValue(t2,"dirty level");*/
-        
+      
         FunctionBlock functionBlock = fis.getFunctionBlock(null);
         // Show 
         JFuzzyChart.get().chart(functionBlock);
 
         // Set inputs
         functionBlock.setVariable("weight", 8);
-        functionBlock.setVariable("dirty_level", 10);
-        //functionBlock.setVariable("weight", Double.parseDouble(t1.getText()));
-        //functionBlock.setVariable("dirty_level", Double.parseDouble(t2.getText()));
+        functionBlock.setVariable("dirty_level", 1);
 
         // Evaluate
         functionBlock.evaluate();
@@ -52,26 +39,4 @@ public class FuzzyLogic {
         // Print ruleSet
         System.out.println(functionBlock);
     }
-    
-    public static void setValue(JTextField tf,String type){
-        frame = new JFrame();
-        double x = 0;
-        String str = "";
-        while(str.equals("")){
-            str = JOptionPane.showInputDialog("Insert your " + type);
-            try{
-                x = Double.parseDouble(str);
-            }catch(NumberFormatException e){
-                JOptionPane.showMessageDialog(frame,"Invalid Input type " + e,"Invalid type", JOptionPane.ERROR_MESSAGE);
-                str = "";
-            }
-            if(x > 10 || x < 0){
-                JOptionPane.showMessageDialog(frame, "Input out of range only 0-10 are allowed","Out of range",JOptionPane.ERROR_MESSAGE);
-                str = "";
-            }else{}
-        }
-        System.out.println(x);
-        tf.setText(x + "");
-    }
-    
 }
